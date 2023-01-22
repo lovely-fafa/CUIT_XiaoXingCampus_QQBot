62d390fe72834f9b8d5736ae75da075d2db4d2b6
fafa <13619088487@163.com>
2023-01-22 20:09:22 +0800

    V2.0

--- code/src/plugins/sendMessageToGroup/__init__.py
+++ code/src/plugins/sendMessageToGroup/__init__.py
@@ -6,34 +6,45 @@
 # @QQ      : 1315337973
 # @File    : sendMessageToGroup.py
 # @Software: PyCharm
-
+import os
 # 可参考插件商店的 nonebot_plugin_forwarder 转发姬插件
 from time import localtime, sleep
 from random import random
 from datetime import datetime
+from io import BytesIO
 
+import requests
 from nonebot import logger
-from nonebot import on_message
-from nonebot.adapters.onebot.v11 import Bot, PrivateMessageEvent
-import asyncio
+from nonebot import on_message, on_notice, on_regex
+from nonebot.adapters.onebot.v11 import Bot, PrivateMessageEvent, FriendRecallNoticeEvent, Event, NoticeEvent, Message
+
 
 from . import config
 
 initial_day = datetime.now().day
 initial_id = 0
-msg_matcher = on_message(priority=10, block=False)
+
+msg_dict = {}  # 显然这个地方是很笨的方法 我们应该存数据库 但是我不会
+
+help_msg_matcher = on_regex(r'^帮助$', priority=5, block=True)
 
 
-async def send_msg_to_group(bot: Bot, group_id: str, msg: str, from_id: str, msg_id: str, from_nickName: str):
-    msg_send = f'{msg_id}.{msg}\n来源：{from_nickName}【{from_id}】'
-    logger.debug(f'消息转发至：{group_id}')
-    await bot.send_group_msg(group_id=int(group_id),
-                             message=msg_send,
-                             auto_escape=False)
+@help_msg_matcher.handle()
+async def _():
+    await help_msg_matcher.finish(Message(config.help_msg))
 
 
-@msg_matcher.handle()
-async def _(bot: Bot, event: PrivateMessageEvent):
+send_msg_matcher = on_message(priority=10, block=False)  # 优先级数字越小越先响应
+
+
+@send_msg_matcher.handle()
+async def matching_send_msg(bot: Bot, event: PrivateMessageEvent):
+    """
+    匹配私聊消息 并转发
+    :param bot:
+    :param event:
+    :return:
+    """
 
     global initial_day
     global initial_id
@@ -52,21 +63,61 @@ async def _(bot: Bot, event: PrivateMessageEvent):
     nickName = event.sender.nickname
 
     logger.debug(f'预转发消息：{msg} | 来源：{from_id}')
-    for gid in config.forwarder_destination_group:
-        await send_msg_to_group(bot=bot,
-                                group_id=gid,
-                                msg=msg,
-                                from_id=from_id,
-                                msg_id=str(initial_id),
-                                from_nickName=nickName)
-        sleep(random())
-    # tasks = [send_msg_to_group(bot=bot,
-    #                            group_id=gid,
-    #                            msg=msg,
-    #                            from_id=from_id,
-    #                            msg_id=str(initial_id)) for gid in config.forwarder_destination_group]
-    # await asyncio.wait(tasks)
 
+    for gid in config.forwarder_destination_group:
+
+        msg_send = f'{initial_id}.{msg}\n提交来源：{nickName}\n提交QQ：{from_id}'
+        logger.debug(f'消息转发至：{gid}')
+        await bot.send_group_msg(group_id=int(gid),
+                                 message=msg_send,
+                                 auto_escape=False)
+        sleep(random())
+
+
+file_msg_matcher = on_notice(priority=16, block=True)
+
+
+@file_msg_matcher.handle()
+async def _(bot: Bot, notice: NoticeEvent):
+    """
+    群文件转发
+    :param bot:
+    :param notice:
+    :return:
+    """
+    # 发送的是文件
+    if notice.notice_type == 'offline_file':
+        print(dir(notice.get_user_id))
+        file_name = f'_by_{notice.user_id}.'.join(notice.file['name'].rsplit('.', 1))
+        file_url = notice.file['url']
+
+        resp = requests.get(file_url)
+        with open(f'./data/sendMessageToGroup/file/{file_name}', mode='wb') as f:
+            f.write(resp.content)
+
+        # 接下来直接调用的 go-cqhttp 的接口 具体在 README.md 里面有讲
+        for gid in config.forwarder_destination_group:
+            await bot.call_api('upload_group_file',
+                               group_id=gid,
+                               file=f'{os.path.abspath(f"./data/sendMessageToGroup/file/{file_name}")}',
+                               name=file_name)
+
+
+# recall_msg_matcher = on_notice(priority=1, block=False)  # tmd 这个地方 撤回是 notice 事件 怎么会是 on_message 啊
+# @recall_msg_matcher.handle()
+# async def matching_recall_msg(bot: Bot, event: FriendRecallNoticeEvent):
+#     """
+#     消息撤回函数
+#     :param bot:
+#     :param event:
+#     :return:
+#     """
+#
+#     msg_id = event.message_id
+#
+#     print(msg_id)
+#     sleep(1)
+#     await bot.delete_msg(message_id=msg_id)
 
 
 
